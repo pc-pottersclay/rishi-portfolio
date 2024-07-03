49ecc16f2242113662c117feb54fdf4a4a2adb60
Maheep Gupta <maheepgupta321@gmail.com>
2024-07-03 23:17:39 +0530

    project link added

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -68,9 +68,9 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple" >
+                  <a className="flex lg:text-xl md:text-xs text-sm text-purple" href={item.link} >
                     Check Live Site
-                  </p>
+                  </a>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
