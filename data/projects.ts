10637affc9e3ab41faf1543eae25fee67b3fb7ce
Maheep Gupta <maheepgupta321@gmail.com>
2024-07-03 21:41:08 +0530

    nike added

--- data/projects.ts
+++ data/projects.ts
@@ -11,7 +11,7 @@ export const projects = [
       id: 2,
       title: "Nike",
       des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
+      img: "/nike.jpeg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
       link: "https://github.com/adrianhajdin/zoom-clone",
     },
